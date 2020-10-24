5bd13c694904b2645ec39f64ea6d1f06ac5fa94c
parsh96 <shah.parshva@ymail.com>
2020-10-24 10:12:48 +0530

    Update variables.tf

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,8 +1,6 @@
-variable "access_key" {
-    default = "AKIAVJTX2WDKZA4MHOJM"    
+variable "access_key" {   
 }
 variable "secret_key" {
-    default = "gMbNPTtyYVFaSpLxDtX5q/UGUuB7lfUDjS9EcJYa"    
 }
 variable "region" {
     default = "ap-southeast-1"
