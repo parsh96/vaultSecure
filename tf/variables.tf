1a708276dd6ada80a1ae7f6369c2710864285889
abilash222 <abilash_222@yahoo.co.in>
2020-10-26 20:16:43 +0800

    update

--- tf/variables.tf
+++ tf/variables.tf
@@ -5,7 +5,7 @@ variable "ami" {
     default = "ami-015a6758451df3cb9"
 }
 variable "hello_tf_instance_count" {
-    default = 1
+    default = 0
 }
 variable "hello_tf_instance_type" {
     default = "t2.micro"
